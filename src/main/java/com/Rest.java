d28a4b64083f49a0135eb76557ecc874bd968bf1
gadiel <ido.gadiel@hpe.com>
2017-11-09 13:44:44 +0200

    commit msg

--- src/main/java/com/Rest.java
+++ src/main/java/com/Rest.java
@@ -2,10 +2,7 @@ package com;
 
 import org.json.JSONObject;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 /*
@@ -33,9 +30,9 @@ public class Rest {
 	//example: http://localhost:9999/home/googleAnalytics/2017-11-03/2017-11-09/
 
 	@GET
-	@Path("googleAnalytics/{startDate}/{endDate}")
+	@Path("googleAnalytics")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getGoogleAnalytics(@PathParam("startDate") String startDate, @PathParam("endDate") String endDate) {
+	public String getGoogleAnalytics(@QueryParam("start-date") String startDate, @QueryParam("end-date") String endDate) {
 		GoogleAnalyticsHelper googleAnalyticsHelper = new GoogleAnalyticsHelper();
 		JSONObject report = googleAnalyticsHelper.getReport(startDate, endDate);
 		return report.toString();
